17f22c697a3f42421b857cf99455f2deaf5bf2c0
tahreen <tahreensultana19@gmail.com>
2024-12-01 20:38:26 -0500

    added db in App.jsx1

--- src/App.jsx
+++ src/App.jsx
@@ -314,9 +314,13 @@ import { BeatLoader } from "react-spinners";
 import { createClient } from "@supabase/supabase-js";
 import { Link } from 'react-router-dom';
 
-const supabaseUrl = import.meta.env.REACT_APP_SUPABASE_URL; 
-const supabaseKey = import.meta.env.REACT_APP_SUPABASE_ANON_KEY; 
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+// const supabaseUrl = import.meta.env.REACT_APP_SUPABASE_URL; 
+// const supabaseKey = import.meta.env.REACT_APP_SUPABASE_ANON_KEY; 
+// const supabase = createClient(supabaseUrl, supabaseKey);
 
 const App = () => {
   const [formData, setFormData] = useState({
